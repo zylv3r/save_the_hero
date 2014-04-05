7a7e2a8fc4e42fe29248c69f3d6a41f5d3463536
zylv3r <ramiro.enriquez@gmail.com>
2014-04-05 11:49:50 -0400

    initial files

---
+++ Assets/Scripts/LimitVelocity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitVelocity : MonoBehaviour {
+    public float topSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (rigidbody2D.velocity.magnitude > topSpeed)
+            rigidbody2D.velocity = rigidbody2D.velocity.normalized * topSpeed;
+	}
+}
